742df331c592e9a0ff4cd099420082c034f1e84f
Rodrigo Delaporte <rod.delaporte@gmail.com>
2021-12-31 20:47:08 -0300

    Static and Media Files: Missing Csrf trusted origin Django 4.0

--- server/zapping/settings.py
+++ server/zapping/settings.py
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'content',
 ]
 
 MIDDLEWARE = [
@@ -109,6 +111,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
